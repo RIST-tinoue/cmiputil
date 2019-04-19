08f9e661f7405d5b34ac96d85d23527d77f9c8c6
Takahiro INOUE <tinoue@rist.jp>
2019-04-19 13:32:40 +0900

    Re-implment convoc as a class ConVoc.

--- convoc.py
+++ convoc.py
@@ -11,125 +11,149 @@ import os.path
 import json
 from pprint import pprint
 
-DEFAULT_CVPATH = "./:./CMIP6_CVs:~/Data/CMIP6/CMIP6_CVs"
 
-def getCVPaths(paths=None):
+class ConVoc:
     """
-    Return list of directories for searching CV json files.
-
-    Directories taken from the environment variable `CVPATH`, which is
-    a string of paths joined by ':'.
-
-    If `CVPATH` is not set, `convoc.DEFAULT_CVPATH` is used.
-
-    If `paths` given, it is used instead of CVPATH.
-
-    Non-existent directories are omitted silently.
-
-    You have to specify '.' explicitly.
-
-    Note that the order is meaningful.
-
-    Parameters
-    ---------
-      paths(optional) : str, paths joined by ':'
-
-    Returns
-    -------
-      List of str
-
+    Class for accessing CMIP6 Controlled Vocabularies
     """
 
-    if (paths is None):
-        cvd = os.environ.get('CVPATH',DEFAULT_CVPATH).split(':')
-    else:
-        cvd = paths.split(':')
-    # print(cvd)
-    cvd = [os.path.expandvars(d) for d in cvd]
-    # print(cvd)
-    cvd = [os.path.expanduser(d) for d in cvd]
-    # print(cvd)
-    cvd = [d for d in cvd if os.path.exists(d)]
-    return cvd
+    DEFAULT_CVPATH = "./:./CMIP6_CVs:~/Data/CMIP6/CMIP6_CVs"
+
+    def __init__(self, paths=None):
+        """
+        Initialize self.
+
+        See setCVPaths() for arguments.
+        """
+
+        self.setCVPaths(paths)
+        pass
+
+    def setCVPaths(self, paths=None):
+        """
+        Set search path for CV json files.
+
+        Directories taken from the environment variable `CVPATH`, which is
+        a string of paths joined by ':'.
+
+        If `CVPATH` is not set, `convoc.DEFAULT_CVPATH` is used.
+
+        If `paths` given, it is used instead of CVPATH.
+
+        Non-existent directories are omitted silently.
+
+        You have to specify '.' explicitly.
+
+        Note that the order is meaningful.
+
+        Parameters
+        ---------
+          paths(optional) : str, paths joined by ':'
+
+        Returns
+        -------
+          List of str
+
+        """
+
+        if (paths is None):
+            p = os.environ.get('CVPATH',self.DEFAULT_CVPATH).split(':')
+        else:
+            p = paths.split(':')
+
+        p = [os.path.expandvars(d) for d in p]
+        p = [os.path.expanduser(d) for d in p]
+        p = [d for d in p if os.path.exists(d)]
+
+        self.cvpath = p
 
 
-def getCV(attr):
-    """
-    Return CV of given `attr` as a dict.
+    def getCVPaths(self):
+        """
+        Return search paths for CV.
+        """
+        return self.cvpath
 
-    `attr` must be valid and it's json file must be in CVPATH.
 
-    Parameters
-    ----------
-      attr : str
+    def getCV(self, attr):
+        """
+        Return CV of given `attr` as a dict.
 
-    Returns
-    -------
-      dict : whole CV key-values
-    """
+        `attr` must be valid and it's json file must be in CVPATH.
 
-    dirs = getCVPaths()
-    file = 'CMIP6_'+attr+'.json'
+        Parameters
+        ----------
+          attr : str
 
-    fname = None
-    for d in dirs:
-        f = os.path.join(d,file)
-        if (os.path.exists(f)):
-            fname = f
-            break
+        Returns
+        -------
+          dict : whole CV key-values
+        """
 
-    # TODO: should raise some exception if no file found.
-    cv = None
-    if fname is None:
+        if (attr in dir(self)):  # already read
+            cv = getattr(self,attr)
+            return cv[attr]
+
+        file = 'CMIP6_'+attr+'.json'
+
+        fname = None
+        for d in self.cvpath:
+            f = os.path.join(d,file)
+            if (os.path.exists(f)):
+                fname = f
+                break
+
+        # TODO: should raise some exception if no file found.
         cv = None
-    else:
-        with open(fname, 'r') as f:
-            cv = json.load(f)
-
-    return cv
-
-
-def checkCV(value, attr):
-    """
-    Check if given `value` is in CV `attr`.
-
-    Parameters
-    ----------
-      attr : str
-      value : str
-
-    Returns
-    -------
-      logical
-    """
-
-    # TODO: should cache json file?
-    cv = getCV(attr)
-    if cv is None:
-        res = False
-    else:
-        res = value in cv[attr]
-    return res
+        if fname is not None:
+            with open(fname, 'r') as f:
+                cv = json.load(f)
+            setattr(self, attr, cv)
+            return cv[attr]
+        else:
+            return None
 
 
+    def checkCV(self, value, attr):
+        """
+        Check if given `value` is in CV `attr`.
 
+        Parameters
+        ----------
+          attr : str
+          value : str
+
+        Returns
+        -------
+          logical
+        """
+
+        # TODO: should cache json file?
+        cv = self.getCV(attr)
+        if cv is None:
+            res = False
+        else:
+            res = value in cv
+        return res
 
 
 if (__name__ == '__main__'):
 
-    paths = getCVPaths()
-    print(type(paths),type(paths[0]),paths[0])
+    cvs = ConVoc()
 
-    source_id = getCV('source_id')
-    print(len(source_id))
+    print(cvs.getCVPaths())
 
+    source_id = cvs.getCV('source_id')
+    print('len(source_id):',len(source_id))
 
+    # 'source_id' has set above
     attr = 'source_id'
     value = 'MIROC-ES2H'
+    print(value,attr,cvs.checkCV(value, attr))
 
-    print(checkCV(value, attr))
-
+    # 'experiment_id' has not set yet
     attr = 'experiment_id'
     value = 'piControl'
-
-    print(checkCV(value, attr))
+    print(value, attr, cvs.checkCV(value, attr))
+    print(dir(cvs))  # see there is cvs.experiment_id
+    
--- test_convoc.py
+++ test_convoc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- 
 
-import convoc
+from convoc import ConVoc
 import unittest
 import os
 
@@ -13,12 +13,24 @@ class ConVoc_Test(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_init01(self):
+        os.environ['CVPATH'] = '.:./CMIP6_CVs/:$HOME:$HOME/CMIP6_CVs'
+        ref = ['.', "$HOME"]
+        ref = [os.path.expandvars(d) for d in ref]
+
+        cvs = ConVoc()
+
+        self.assertTrue(isinstance(cvs, ConVoc))
+
+
+
     def test_getCVPaths01(self):
         os.environ['CVPATH'] = '.:./CMIP6_CVs/:$HOME:$HOME/CMIP6_CVs'
         ref = ['.', '$HOME']
         ref = [os.path.expandvars(d) for d in ref]
 
-        res = convoc.getCVPaths()
+        cvs = ConVoc()
+        res = cvs.getCVPaths()
 
         self.assertEqual(ref, res)
 
@@ -26,16 +38,19 @@ class ConVoc_Test(unittest.TestCase):
         os.environ['CVPATH'] = '/some/nonexistent/path'
         ref = []
 
-        res = convoc.getCVPaths()
+        cvs = ConVoc()
+        res = cvs.getCVPaths()
 
         self.assertEqual(ref, res)
 
 
     def test_getCVPaths11(self):
+        cvpath = '.:./CMIP6_CVs/:$HOME:$HOME/CMIP6_CVs'
         ref = ['.', '$HOME']
         ref = [os.path.expandvars(d) for d in ref]
 
-        res = convoc.getCVPaths('.:./CMIP6_CVs/:$HOME:$HOME/CMIP6_CVs')
+        cvs = ConVoc(cvpath)
+        res = cvs.getCVPaths()
 
         self.assertEqual(ref, res)
 
@@ -44,31 +59,34 @@ class ConVoc_Test(unittest.TestCase):
         attr = 'activity_id'
         ref = 'CMIP DECK: 1pctCO2, abrupt4xCO2, amip, esm-piControl, esm-historical, historical, and piControl experiments'
 
-        cv = convoc.getCV(attr)
-        res = cv[attr]['CMIP']
+        cvs = ConVoc()
+        cv = cvs.getCV(attr)
+        res = cv['CMIP']
 
-        self.assertEqual(len(cv),2)
-        self.assertEqual(len(cv[attr]),24)
+        self.assertEqual(len(cv),24)
         self.assertEqual(ref, res)
 
     def test_getCV02(self):
         attr = 'invalid_id'
 
-        cv = convoc.getCV(attr)
+        cvs = ConVoc()
+        cv = cvs.getCV(attr)
         self.assertTrue(cv is None)
 
     def test_checkCV01(self):
         attr = 'activity_id'
         value = 'CMIP'
 
-        res = convoc.checkCV(value, attr)
+        cvs = ConVoc()
+        res = cvs.checkCV(value, attr)
         self.assertTrue(res)
 
     def test_checkCV02(self):
         attr = 'activity_id'
         value = 'invalid_value'
 
-        res = convoc.checkCV(value, attr)
+        cvs = ConVoc()
+        res = cvs.checkCV(value, attr)
         self.assertFalse(res)
 
 
