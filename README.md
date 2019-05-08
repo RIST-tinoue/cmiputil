b5a1e1fd8370031b6a62d5137bc1303d6f145e55
Takahiro INOUE <tinoue@rist.jp>
2019-05-08 10:39:38 +0900

    start up gh-pages.

--- README.md
+++
@@ -1,4 +0,0 @@
-# cmiputil
-
-This is a small package for CMIP6/ESGF users, especially for myself.
-
--- cmiputil/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import convoc
-from . import drs
-from . import esgfsearch
-
-__version__ = '0.1'
