51b22e8f79f6a18a7920dc3d67f91d1e7892b55d
RIST-tinoue <49736919+RIST-tinoue@users.noreply.github.com>
2019-05-31 20:04:44 +0900

    Created createSampleConf.

---
+++ bin/createSampleConf.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Create config file for cmiputil.
+"""
+
+import cmiputil
+import argparse
+
+__author__ = 'T.Inoue'
+__credits__ = 'Copyright (c) 2019 RIST'
+__version__ = 'v20190530'
+__date__ = '2019/05/30'
+
+
+desc = __doc__
+epilog = """
+Note that the default search path for config file is current directory
+and $HOME in this order, with filename 'cmiputil.conf'.
+"""
+
+
+def my_parser():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=desc,
+        epilog=epilog)
+    parser.add_argument(
+        'file', type=str, nargs='?', metavar='conf_file',
+        default='cmiputil.conf')
+    parser.add_argument(
+        '-o', '--overwrite', action='store_true', default=False)
+
+    return parser
+
+
+if __name__ == '__main__':
+
+    parser = my_parser()
+    args = parser.parse_args()
+
+    conf = cmiputil.config.Conf('')
+    conf.setDefaultSection()
+
+    try:
+        d = cmiputil.esgfsearch.getDefaultConf()
+    except AttributeError:
+        pass
+    else:
+        conf.read_dict(d)
+
+    try:
+        d = cmiputil.drs.getDefaultConf()
+    except AttributeError:
+        pass
+    else:
+        conf.read_dict(d)
+
+    try:
+        d = cmiputil.convoc.getDefaultConf()
+    except AttributeError:
+        pass
+    else:
+        conf.read_dict(d)
+
+    conf.writeSampleConf(args.file, overwrite=args.overwrite)
+    print(f'config file "{args.file}" created successfully')
