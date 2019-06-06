5e3edcd4b307ad667cafcf9274917febfe59e519
RIST-tinoue <49736919+RIST-tinoue@users.noreply.github.com>
2019-06-06 16:44:00 +0900

    Changed overwrite option from -o to -O.

--- bin/createSampleConf.py
+++ bin/createSampleConf.py
@@ -14,10 +14,7 @@ __date__ = '2019/05/30'
 
 
 desc = __doc__
-epilog = """
-Note that the default search path for config file is current directory
-and $HOME in this order, with filename 'cmiputil.conf'.
-"""
+epilog = ""
 
 
 def my_parser():
@@ -29,15 +26,12 @@ def my_parser():
         'file', type=str, nargs='?', metavar='conf_file',
         default='cmiputil.conf')
     parser.add_argument(
-        '-o', '--overwrite', action='store_true', default=False)
-
+        '-O', '--overwrite', action='store_true', default=False)
     return parser
 
 
-if __name__ == '__main__':
-
-    parser = my_parser()
-    args = parser.parse_args()
+def main():
+    a = my_parser().parse_args()
 
     conf = cmiputil.config.Conf('')
     conf.setDefaultSection()
@@ -63,5 +57,9 @@ if __name__ == '__main__':
     else:
         conf.read_dict(d)
 
-    conf.writeSampleConf(args.file, overwrite=args.overwrite)
-    print(f'config file "{args.file}" created successfully')
+    conf.writeSampleConf(a.file, overwrite=a.overwrite)
+    print(f'config file "{a.file}" created successfully')
+
+
+if __name__ == '__main__':
+    main()
